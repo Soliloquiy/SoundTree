bc547f30a77cd512e8535ff7a2584a2c2e8ba57b
jojo2829 <joulinli@gmail.com>
2021-07-31 21:54:32 +0000

    Post - class name change

--- client/src/components/forum/Post.js
+++ client/src/components/forum/Post.js
@@ -6,11 +6,11 @@ export default function Post(props) {
 
   return (
     <article>
-      <div className="post_user_profile">
-        <img className="post_user_avatar" src="https://img.icons8.com/ios/452/spongebob-squarepants.png" />
+      <div className="post-user-profile">
+        <img className="post-user-avatar" src="https://img.icons8.com/ios/452/spongebob-squarepants.png" />
         <span>Spongebob223</span>
       </div>
-      <div className="post_content">
+      <div className="post-content">
         <p>I love this song, it's amazing.</p>
         <LikeCount />
       </div>
