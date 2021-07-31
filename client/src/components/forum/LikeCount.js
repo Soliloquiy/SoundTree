fe6a427f88370247653f915153369624ceb4aed0
jojo2829 <joulinli@gmail.com>
2021-07-31 19:00:00 +0000

    post component - like count component

---
+++ client/src/components/forum/LikeCount.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LikeCount.scss"
+
+export default function LikeCount() {
+
+  return (
+    <div className="like_count">
+    <button className="like_button"> Like </button>
+    <span>50</span>
+    </div>
+  )
+};
