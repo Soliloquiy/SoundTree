b2c0e5d8c8682ae4ba091f258a923d6e39f3989f
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-08-01 19:51:57 +0000

    Add enviroment vairiables for spotify api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,7 @@ class Api::UsersController < ApplicationController
 
   # Now you can access playlists in detail, browse featured content and more
   def index
-    RSpotify.authenticate(CLIENT_ID, CLIENT_SECRET)
+    RSpotify.authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
     @playlists = RSpotify::User.find('phil_bsg').playlists
     render json: @playlists
   end
--- config/initializers/spotify_setup.rb
+++ config/initializers/spotify_setup.rb
@@ -1,3 +1,5 @@
 require 'rspotify'
 
-RSpotify::authenticate(CLIENT_ID, CLIENT_SECRET)
+RSpotify::authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
+
+#RSpotify::authenticate('ccc7dcccd4db4eed86deae10a83f6c52', 'b3bf6ac403894e11bc8855daf7098d9a')
