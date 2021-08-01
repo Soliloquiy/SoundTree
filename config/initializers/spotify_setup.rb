806deeddd2c2001140e122cdd3aa228ee273fc8b
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-08-01 20:30:40 +0000

    Add environment variables with Figaro for Spotify authentication

--- config/initializers/spotify_setup.rb
+++ config/initializers/spotify_setup.rb
@@ -1,5 +1,3 @@
 require 'rspotify'
 
-RSpotify::authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
-
-#RSpotify::authenticate('ccc7dcccd4db4eed86deae10a83f6c52', 'b3bf6ac403894e11bc8855daf7098d9a')
+RSpotify::authenticate(ENV["client_id"], ENV["client_secret"])
