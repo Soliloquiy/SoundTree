4d318078439ca57ae4f8301b953bdaa7f883f9a4
jojo2829 <joulinli@gmail.com>
2021-08-03 20:13:14 +0000

    register - user input validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ApplicationRecord
   has_many :subgenre, through: :usergenre
   has_many :post
 
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  validates :password, presence: true
   
   # has_secure_password
 
   def self.authenticate_with_credentials(email, password)
     user = User.find_by(:email => email.strip.downcase)
 
-    if user && user.authenticate(password)
+    if user #&& user.authenticate(password)
       user
     else
       nil
