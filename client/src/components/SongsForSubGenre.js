6188c82866313e7aceec1b659462b964b1f19742
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-08-10 01:57:21 +0000

    Added player functionality to homepage

--- client/src/components/SongsForSubGenre.js
+++ client/src/components/SongsForSubGenre.js
@@ -1,39 +1,41 @@
 import React from "react";
-
+import Player from "./Player";
 
 // const genreClass = classNames("genre-list__item", {
-    //   "genre-list__item--selected": props.selected,
-    // });
-
-
-
+//   "genre-list__item--selected": props.selected,
+// });
 
 export default function SongsForSubGenre(props) {
-  
-  console.log(props.subGenre)
-  console.log(props.songs)
+  console.log(props.subGenre);
+  console.log(props.songs);
 
   if (!props.subGenre) {
-    return []
+    return [];
   } else
-  return (
-    <ul>
-      {props.songs.map((item) => {
-        return (
-          <div className="App">
-            <br></br>
-            <li key={item.id}>{item.name}</li>
-            { item.songs.map((song) => 
-            <div>
-            <li key={song.id}>
-              Song Name: {song.name} 
-              Song Artist: {song.artist} 
-              Song Album: {song.album} 
-            </li>
-            </div>)}
-          </div>
-        );
-      })}
-    </ul>
-  );
+    return (
+      <div>
+        <ul>
+          {props.songs.map((item) => {
+            return (
+              <div className="App">
+                <br></br>
+                <li key={item.id}>{item.name}</li>
+                {item.songs.map((song) => (
+                  <div>
+                    <li key={song.id}>
+                      Song Name: {song.name}
+                      Song Artist: {song.artist}
+                      Song Album: {song.album}
+                    </li>
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </ul>
+        <div className="player">
+          <Player token={props.token} uris={props.uris} />
+        </div>
+      </div>
+    );
 }
